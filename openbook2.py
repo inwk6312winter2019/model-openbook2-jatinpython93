93132af4e7f816a86e13569dbadad2d3c68f4173
jatinpython93 <jt519510@dal.ca>
2019-02-06 20:45:57 -0400

    Open Book edited and done

--- openbook2.py
+++ openbook2.py
@@ -1,9 +1,38 @@
-def tuple_required(file_name):
-	fin = open(file_name,'r')
-	for line in fin:
-		line = line.split(",")
-		tup = (line[2],line[4],line[6],line[7])
-		print(tup)
+import csv
 
-tuple_required('Street_Centrelines.csv')
+def task2():    
+	street = []  
+	nstreet = []    
+	reader1 = open('Street_Centrelines.csv', 'r')    
+	reader2 = open('Bus_Stops.csv', 'r')    
+	for rea in reader1:        
+		nrea = rea.split(",")        
+		if nrea[10] == "ARTERIAL":            
+			stee = nrea[4].strip()            
+			stee = stee.lower()            
+			if stee not in street:                 
+				street.append(stee)    
+	for stre in street:
+		if stre not in nstreet:            
+			 nstreet.append(stre)
 
+
+	metro = []    
+	for st in nstreet:        
+		for der in reader2:            
+			nder = der.split(",")            
+			if nder[7] == "Accessible":                
+				haystack = nder[6].strip()                
+				haystack = haystack.lower()                
+				if haystack.find(st) >= 0:                     
+					metro.append(nder[2])
+
+	print("StopNumber|Location|FCODE")
+
+	for bus in metro:        
+		for re in open('Bus_Stops.csv', 'r'):           
+			nre = re.split(",")            
+			if nre[2].find(bus) >= 0:                
+				print(nre[4],"",nre[6],"",nre[10])
+
+task2()
